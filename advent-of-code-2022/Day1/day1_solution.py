69af6be521a329d62e918e912ae49e4b99fc882a
AleksaMCode <aleksamcode@gmail.com>
2022-12-01 09:14:29 +0100

    Bug removed

--- advent-of-code-2022/Day1/day1_solution.py
+++ advent-of-code-2022/Day1/day1_solution.py
@@ -1,9 +1,9 @@
 elfs = []
 
 
-def parse_elves():
+def parse_elves(file_name: str):
     calories = 0
-    with open("input") as file:
+    with open(file_name) as file:
         for line in file:
             line = line.strip()
             if not line:
@@ -22,6 +22,6 @@ def top3_elves_calories():
     print(sum(elfs[-3:]))
 
 
-parse_elves()
+parse_elves("day1_input")
 top_elf_calories()
 top3_elves_calories()
