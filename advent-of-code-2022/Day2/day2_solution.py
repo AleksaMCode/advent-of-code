4d315303f9fe6415325ba41c995970ca3ed2b01e
AleksaMCode <aleksamcode@gmail.com>
2022-12-02 08:22:48 +0100

    Day 2 solution added

---
+++ advent-of-code-2022/Day2/day2_solution.py
@@ -0,0 +1,43 @@
+# A -       Rock        - X
+# B -       Paper       - Y
+# C -       Scissors    - Z
+class RockPaperScissors:
+    value: int
+
+    def __init__(self, value):
+        self.value = "XYZ".index(value)
+
+    def __eq__(self, other) -> int:
+        match (self.value - "ABC".index(other)) % 3:
+            case 0:
+                return self.value + 1 + 3
+            case 1:
+                return self.value + 1 + 6
+            case _:
+                return self.value + 1
+
+    def __ne__(self, other) -> int:
+        other = "ABC".index(other)
+        match self.value:
+            case 0:
+                return (other - 1) % 3 + 1
+            case 1:
+                return 3 + other + 1
+            case _:
+                return 6 + (other + 1) % 3 + 1
+
+    @staticmethod
+    def parse_input(file_name):
+        total_score1 = 0
+        total_score2 = 0
+        with open(file_name) as file:
+            for line in file:
+                values = line.strip().split(' ')
+                opponent = values[0]
+                you = RockPaperScissors(values[1])
+                total_score1 += you == opponent
+                total_score2 += you != opponent
+        print(total_score1, total_score2)
+
+
+RockPaperScissors.parse_input('day2_input')
