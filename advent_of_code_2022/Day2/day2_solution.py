32df5af0ae36356521ec986616dbb6729df2b964
AleksaMCode <aleksamcode@gmail.com>
2022-12-02 15:57:20 +0100

    Code refactored

--- advent_of_code_2022/Day2/day2_solution.py
+++ advent_of_code_2022/Day2/day2_solution.py
@@ -27,6 +27,14 @@ class RockPaperScissors:
                 return 6 + (other + 1) % 3 + 1
 
     @staticmethod
+    def function1(you, opponent):
+        return you == opponent
+
+    @staticmethod
+    def function2(you, opponent):
+        return you != opponent
+
+    @staticmethod
     def parse_input(file_name):
         total_score1 = 0
         total_score2 = 0
@@ -35,9 +43,6 @@ class RockPaperScissors:
                 values = line.strip().split(' ')
                 opponent = values[0]
                 you = RockPaperScissors(values[1])
-                total_score1 += you == opponent
-                total_score2 += you != opponent
+                total_score1 += RockPaperScissors.function1(you, opponent)
+                total_score2 += RockPaperScissors.function2(you, opponent)
         print(total_score1, total_score2)
-
-
-RockPaperScissors.parse_input('day2_input')
